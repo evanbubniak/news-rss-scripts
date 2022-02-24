8ba74ec7f3bafab7d9165cfdb150f136db4f1c01
Evan Bubniak <evan.bubniak@gmail.com>
2022-02-24 11:24:36 -0500

    add some type hints

--- article.py
+++ article.py
@@ -1,6 +1,6 @@
 class Article:
 
-    def __init__(self, title = "", short_title = None, content = None, url = None):
+    def __init__(self, title: str = "", short_title = None, content = None, url = None):
         self.title = title
         self.short_title = short_title
         self.content = content
@@ -9,14 +9,14 @@ class Article:
     def __repr__(self) -> str:
         return f"Title: {self.get_title()}\nContent: {self.get_content()}\nURL: {self.get_url()}"
 
-    def get_content(self):
+    def get_content(self) -> str:
         return self.content
 
-    def get_title(self):
+    def get_title(self) -> str:
         return self.title if self.title else self.short_title
 
-    def get_short_title(self):
+    def get_short_title(self) -> str :
         return self.short_title if self.short_title else self.title
 
-    def get_url(self):
+    def get_url(self) -> str:
         return self.url
