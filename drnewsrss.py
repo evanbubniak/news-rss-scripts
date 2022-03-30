a9227b5ae2d4e28722bbf81bea2b412c43fdec85
Evan Bubniak <evan.bubniak@gmail.com>
2022-03-30 10:29:12 -0400

    correct dr parsing

--- drnewsrss.py
+++ drnewsrss.py
@@ -6,11 +6,10 @@ from get_url_soup import get_url_soup
 class DRNewsArticle(RSSArticle):
     def retrieve_content(self):
         soup = get_url_soup(self.url)
-        if "/seneste/" in self.url:
-            body_classname = "hydra-latest-news-page-short-news__body"
-        else:
-            body_classname = "dre-article-body"
-        body_tag = soup.find("div", class_=body_classname)
+        for body_tag_classname in ["dre-article-body", "hydra-latest-news-page-short-news__body"]:
+            body_tag = soup.find("div", class_=body_tag_classname)
+            if body_tag:
+                break
 
         if body_tag:
             text = "<p>" + "</p><p>".join([text_tag.getText() for text_tag in body_tag.find_all(class_ ="dre-speech")]) + "</p>"
