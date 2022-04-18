77744ff01eb2b7c7bceb72ce746409bd819c9d7c
Evan Bubniak <evan.bubniak@gmail.com>
2022-04-17 22:59:57 -0400

    change user agent

--- get_url_soup.py
+++ get_url_soup.py
@@ -6,7 +6,7 @@ def soup(html):
     return BeautifulSoup(html, 'html.parser')
 
 def get_url_soup(url):
-    return soup(get(url).content)
+    return soup(get(url, headers = { "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1" }).content)
 
 def get_requests_html_soup(url):
     session = HTMLSession()
