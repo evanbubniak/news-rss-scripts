17dad4769579063b8683beb7857a80e3444fd624
Evan Bubniak <evan.bubniak@gmail.com>
2022-05-05 21:04:59 -0400

    fix unintended double assignment in headers line

--- get_url_soup.py
+++ get_url_soup.py
@@ -2,7 +2,7 @@ from requests import get
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 
-ua_headers = headers = { "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1" }
+ua_headers = { "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1" }
 
 def soup(html):
     return BeautifulSoup(html, 'html.parser')
